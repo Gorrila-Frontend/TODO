ddd3db2f1de6579f0dd1166538ff54496f6a02af
Anton Lobanov <andrey.tupikin@cronix.ms>
2018-05-30 18:32:44 +0300

    2018

---
+++ src/components/Todo.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Todo = ({ onClick, completed, text }) => (
+  <li
+    onClick={onClick}
+    style={{
+      textDecoration: completed ? 'line-through' : 'none',
+      color: completed ? '#ccc' : '#000',
+      background: completed ? '#222' : 'cornsilk'
+    }}
+  >
+    <p>{text}</p>
+  </li>
+)
+
+Todo.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  completed: PropTypes.bool.isRequired,
+  text: PropTypes.string.isRequired
+}
+
+export default Todo
---
+++ src/components/TodoList.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Todo from './Todo'
+import '../index.css';
+const TodoList = ({ props,todos, toggleTodo }) => (
+  <ul className="ulList">
+    {todos.map(todo =>
+      <Todo
+        key={todo.id}
+        {...todo}
+        onClick={() => toggleTodo(todo.id)}
+      />
+    )}
+  </ul>
+)
+
+TodoList.propTypes = {
+  todos: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    completed: PropTypes.bool.isRequired,
+    text: PropTypes.string.isRequired
+  }).isRequired).isRequired,
+  toggleTodo: PropTypes.func.isRequired
+}
+
+export default TodoList;
